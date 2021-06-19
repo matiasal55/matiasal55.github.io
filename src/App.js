75308224eb64511fd6760534f8694a63e2434c1d
Matias <matiasalarcon55@hotmail.com>
2021-06-18 23:06:18 -0300

    Main hecho

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+import Main from './components/Main';
+
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <Main />
         </div>
     );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import '../styles/Main.scss';
+
+const Main = () => {
+    return (
+        <main>
+            <div className='main'>
+                <div className='menu'>
+                    <h1>Matias Alarcón</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Works</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div className='picture'></div>
+            </div>
+        </main>
+    );
+};
+
+export default Main;

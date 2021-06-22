ee18d068d64676bac9f1998dbde37c0b6d6ed75d
Matias <matiasalarcon55@hotmail.com>
2021-06-22 15:46:58 -0300

    Prueba con suspense

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,26 @@
-import { useEffect, useState } from 'react';
+import { Suspense } from 'react';
 import { css } from '@emotion/react';
 import Loader from 'react-spinners/PropagateLoader';
 import '../styles/Main.scss';
 
 const Main = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setLoading(false);
-    }, []);
-
-    if (loading)
-        return (
-            <div className='loading'>
-                <Loader color='#1ab6b9' />
-            </div>
-        );
-
     return (
-        <main>
-            <div className='main'>
-                <div className='menu'>
-                    <h1>Matias Alarcón</h1>
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Works</li>
-                        <li>Contact</li>
-                    </ul>
+        <Suspense fallback={<Loader color='#1ab6b9' />}>
+            <main>
+                <div className='main'>
+                    <div className='menu'>
+                        <h1>Matias Alarcón</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Works</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                    <div className='picture'></div>
                 </div>
-                <div className='picture'></div>
-            </div>
-        </main>
+            </main>
+        </Suspense>
     );
 };
 

5fbae97ca4c1af514e47c9464655b53c03c50d98
Matias <matiasalarcon55@hotmail.com>
2021-07-25 23:10:40 -0300

    Menu esp eng desktop

--- src/components/MenuLink.js
+++ src/components/MenuLink.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/_menuLink.scss';
 
 const MenuLink = ({ path, esp, eng }) => {
+    const [link, setLink] = useState(esp);
+
+    const mouseOver = () => {
+        setLink(eng);
+    };
+
+    const mouseLeave = () => {
+        setLink(esp);
+    };
+
     return (
-        <Link to={path}>
-            <li>{esp}</li>
+        <Link to={path} onMouseEnter={mouseOver} onMouseLeave={mouseLeave}>
+            <li>{link}</li>
         </Link>
     );
 };

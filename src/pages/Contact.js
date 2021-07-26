f7afa2706d060cf3dbd1299a856739b33d16bf70
Matias <matiasalarcon55@hotmail.com>
2021-07-26 20:02:29 -0300

    Link a Linkedin en contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -51,10 +51,12 @@ const Contact = () => {
             <div>
                 <h2>Contacto / Contact:</h2>
                 <p>
-                    Puedes contactarme haciendo click <a href='mailto:mati.alarcon55@gmail.com'>aquí</a> o completando el siguiente formulario:
+                    Podés contactarme haciendo click <a href='mailto:mati.alarcon55@gmail.com'>aquí</a>. También podés agregarme a tu red en
+                    <a href='https://www.linkedin.com/in/matiasalarcon'> Linkedin</a> o podés completar el siguiente formulario:
                 </p>
                 <p>
-                    If you would like contact me, please make click <a href='mailto:mati.alarcon55@gmail.com'>here</a> or complete the next form:
+                    If you would like contact me, please make click <a href='mailto:mati.alarcon55@gmail.com'>here</a>. Also you can add me to your network
+                    contact in <a href='https://www.linkedin.com/in/matiasalarcon'>Linkedin</a> or you can complete the next form:
                 </p>
                 <form method='POST' onSubmit={handleSubmit(onSubmit)}>
                     <FormField

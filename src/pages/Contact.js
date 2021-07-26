94af9375af88bad50152ecdaff75d9f4498abfe7
Matias <matiasalarcon55@hotmail.com>
2021-07-26 18:06:10 -0300

    Render msg gracias. Agregado campo company

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Content from '../components/Content';
 import FormField from '../components/FormField';
@@ -5,10 +6,19 @@ import '../styles/_contact.scss';
 
 const Contact = () => {
     const { handleSubmit, register } = useForm();
+    const [send, setSend] = useState(false);
 
     const onSubmit = (e) => {
-        console.log(e);
+        setSend(true);
     };
+
+    if (send)
+        return (
+            <Content page='contact'>
+                <h2 className='thanks'>Muchas gracias por enviar su mensaje. En breve será leído y en caso de ser necesario respondido.</h2>
+                <h2 className='thanks'>Thank you so much for send me a message. It will be read shortly and if necessary answered.</h2>
+            </Content>
+        );
 
     return (
         <Content page='contact'>
@@ -22,12 +32,14 @@ const Contact = () => {
                 </p>
                 <form method='POST' onSubmit={handleSubmit(onSubmit)}>
                     <FormField labelFor='name' labelName='Nombre / Name:' placeholder='Ingrese su nombre / Enter your name' register={register} />
+                    <FormField labelFor='company' labelName='Empresa / Company:' placeholder='Opcional / Optional' required={false} register={register} />
                     <FormField
                         labelFor='telephone'
                         labelName='Teléfono / Phone:'
-                        placeholder='Ingrese su teléfono con código de area / Enter your phone with area code'
+                        placeholder='Opcional / Optional'
                         type='tel'
                         register={register}
+                        required={false}
                     />
                     <FormField labelFor='email' labelName='Email:' placeholder='Ingrese su email / Enter your email' type='email' register={register} />
                     <div className='form-group'>

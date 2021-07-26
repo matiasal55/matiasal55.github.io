1305b902199e46d1cac752f4b7751347b0986b4f
Matias <matiasalarcon55@hotmail.com>
2021-07-26 18:05:44 -0300

    Required

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const FormField = ({ labelFor, labelName, placeholder, type = 'text', register }) => {
+const FormField = ({ labelFor, labelName, placeholder, type = 'text', register, required = true }) => {
     return (
         <div className='form-group'>
             <label htmlFor={labelFor}>{labelName}</label>
-            <input id={labelFor} type={type} placeholder={placeholder} {...register(labelFor)} />
+            <input id={labelFor} type={type} placeholder={placeholder} {...register(labelFor, { required })} />
         </div>
     );
 };

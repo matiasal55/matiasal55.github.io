46a43e75038f7c85d2018d7a796cfb0c96a4cc77
Matias <matiasalarcon55@hotmail.com>
2021-07-26 12:33:32 -0300

    Boton cerrar

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
 import '../styles/_content.scss';
 
 const Content = ({ children }) => {
-    return <div className='content'>{children}</div>;
+    return (
+        <div className='content'>
+            <div className='close'>
+                <Link to='/'>CERRAR</Link>
+            </div>
+            <div>{children}</div>
+        </div>
+    );
 };
 
 export default Content;

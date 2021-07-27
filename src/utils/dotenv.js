2b47320cb1e1c7b120f49b3f02f11393e99c1790
Matias <matiasalarcon55@hotmail.com>
2021-07-26 23:46:50 -0300

    Asignacion de captcha key

--- src/utils/dotenv.js
+++ src/utils/dotenv.js
@@ -4,4 +4,5 @@ module.exports = {
     serviceID: process.env.REACT_APP_EMAIL_SERVICE_ID,
     templateID: process.env.REACT_APP_EMAIL_TEMPLATE_ID,
     userID: process.env.REACT_APP_EMAIL_USERID,
+    captchaID: process.env.REACT_APP_CAPTCHA_KEY,
 };

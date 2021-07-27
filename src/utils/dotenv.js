aaf9add1a86f5942f2a12a904cf39ad960e19609
Matias <matiasalarcon55@hotmail.com>
2021-07-26 21:30:56 -0300

    Variables de entorno en archivo aparte

---
+++ src/utils/dotenv.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv').config();
+
+module.exports = {
+    user: process.env.REACT_APP_EMAIL_USER,
+    password: process.env.REACT_APP_EMAIL_PASSWORD,
+    host: process.env.REACT_APP_EMAIL_HOST,
+};

938bcbc8dbce70b2d51daa401b9d1ab8ef326235
Matias <matiasalarcon55@hotmail.com>
2021-07-27 10:14:10 -0300

    Uso de componente ErrorMsg

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,11 +1,12 @@
 import '../styles/_form-field.scss';
+import ErrorMsg from './ErrorMsg';
 
 const FormField = ({ labelFor, labelName, placeholder, type = 'text', register, required = true, errors }) => {
     return (
         <div className='form-group'>
             <label htmlFor={labelFor}>{labelName}</label>
             <input id={labelFor} type={type} placeholder={placeholder} {...register(labelFor, { required })} />
-            {errors[labelFor] ? <div className='error'>{errors[labelFor].message}</div> : null}
+            <ErrorMsg error={errors[labelFor]} />
         </div>
     );
 };

e8bc7c00b63d8f9817fd68e9d2cedf1be0be7871
Matias <matiasalarcon55@hotmail.com>
2021-07-26 22:20:16 -0300

    Funcion que envia email

---
+++ src/utils/email.js
@@ -0,0 +1,12 @@
+import emailjs from 'emailjs-com';
+import { serviceID, templateID, userID } from './dotenv';
+
+export const sendEmail = async (data) => {
+    try {
+        const send = await emailjs.send(serviceID, templateID, data, userID);
+        return true;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+};

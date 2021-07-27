eba523d3854ca490f2b5a1b3af0c71a37734888b
Matias <matiasalarcon55@hotmail.com>
2021-07-27 11:23:48 -0300

    Pagina Works basica

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from './components/Main';
 import Index from './pages/Index';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Works from './pages/Works';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Switch>
                     <Route exact path='/' component={Index} />
                     <Route exact path='/about' component={About} />
+                    <Route exact path='/works' component={Works} />
                     <Route exact path='/contact' component={Contact} />
                 </Switch>
             </Main>
---
+++ src/pages/Works.js
@@ -0,0 +1,11 @@
+import Content from '../components/Content';
+
+const Works = () => {
+    return (
+        <Content>
+            <h2>Works</h2>
+        </Content>
+    );
+};
+
+export default Works;
